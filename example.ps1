00682a95fe5441d64e0b2a64a190a61ceff3cf91
Ireneusz Opaliński <82705344+Irek-h@users.noreply.github.com>
2022-01-30 11:58:58 +0100

    fix example.ps1

--- example.ps1
+++ example.ps1
@@ -1,15 +1,18 @@
 $path = $env:USERPROFILE + "\Downloads"
+#$path = "C:\Temp"
 
 $url = "https://download.virtualbox.org/virtualbox/6.1.30/VirtualBox-6.1.30-148432-Win.exe"
-$hash = "1d085322564f039bf4f46fcb42735516b1a04629ec83da5b97f759e27fecbcdf"
 
 Invoke-WebRequest $url -OutFile "$path\VirtualBox-6.1.30-148432-Win.exe"
-Set-Location $path
-$hash_compare = ($hash -eq (Get-FileHash -Algorithm SHA256 .\VirtualBox-6.1.30-148432-Win.exe | Format-List | Out-String).Trim().split([Environment]::NewLine)[2].split(':')[1].Trim().ToLower())
-if($hash_compare){
-    Write-Host "I Worekd!"
-    # Install Software that was checked
+
+$hash = "1d085322564f039bf4f46fcb42735516b1a04629ec83da5b97f759e27fecbcdf" # Hash provided by producer
+$calculated_hash = (Get-FileHash -Algorithm SHA512 $path\VirtualBox-6.1.30-148432-Win.exe).Hash.ToLower()
+
+if($hash -eq $calculated_hash){
+    # Install Software
+    Exit 0
 }
 else {
-    Write-Host "HASH ERROR!"
+    Write-Error "Hash error"
+    Exit 1
 }
